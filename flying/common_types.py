fc4fae1871a6239c01924659cd4b7463b96985ae
Hoang Ngoc Tran <emailngochoang@gmail.com>
2019-01-11 11:32:53 +0100

    flying & training code

---
+++ flying/common_types.py
@@ -0,0 +1,27 @@
+CENTER_POS = 0
+SOUTH_EAST_POS = 1
+NORTH_EAST_POS = 2
+NORTH_WEST_POS = 3
+SOUTH_WEST_POS = 4
+SOUTH_POS = 5
+EAST_POS = 6
+NORTH_POS = 7
+WEST_POS = 8
+
+INSPECTION_X = 2.25
+INSPECTION_Y = 2.25
+INSPECTION_Z = 1.5
+
+NORTH_SOUTH_DRT = 1
+SOUTH_NORTH_DRT = 2
+EAST_WEST_DRT = 3
+WEST_EAST_DRT = 4
+
+NAVIGATION_SPEED = 0.9
+INSPECTION_SPEED = 0.6
+
+# stop before reaching to the final destination 0.5m because the momentum make the drone not able to stop right away
+STOP_DISTANCE = 0.5 
+FINAL_DISTANCE = 0.5
+GOING_WAY = 1
+RETURN_WAY = 2
